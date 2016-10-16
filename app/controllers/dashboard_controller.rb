2e14c1b83fd219147ec6de91896e4b10361f87d1
Rittidate <arraieot@gmail.com>
2016-10-17 09:03:26 +1100

    update dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,16 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create]
   
   def index
+    user_ids = feed_message_user
+    @feed_message = FeedMessage.where(sender_id: user_ids).order("created_at DESC")
+  end
+private
+  def feed_message_user
+    user_array = Array.new
+    for f in current_user.follows
+      user_array << f.follower.id
+    end
+
+    user_array << current_user.id
   end
 end
