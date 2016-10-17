84ed75250d5450b30d0f050030f9f3de9f84a0b9
Rittidate <arraieot@gmail.com>
2016-10-17 18:39:45 +1100

    update content type

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -19,5 +19,5 @@ class Resource < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :document
-  validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+  validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 end
